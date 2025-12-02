4c34bb8a97ad150248d1dacfb256e2c651242ee0
Sabrina Meindlhumer <58905866+M-Sabrina@users.noreply.github.com>
2025-12-02 20:31:18 +0100

    day 02 part 2 C#

--- day02_cs/Program.cs
+++ day02_cs/Program.cs
@@ -37,6 +37,50 @@
     }
 }
 
+static void Part2(string file)
+{
+    // To read a text file line by line
+    if (File.Exists(file))
+    {
+        long result = 0;
+
+        var input = File.ReadAllText(file);
+        var ranges = input.Split(",");
+
+        foreach (string range in ranges)
+        {
+            var minMax = range.Split("-");
+            long min = Convert.ToInt64(minMax[0]);
+            long max = Convert.ToInt64(minMax[1]);
+
+            long count = min;
+            while (count <= max)
+            {
+                string number = count.ToString();
+                int length = number.Length;
+                for (int patternLen = 1; patternLen <= length / 2; patternLen++)
+                {
+                    if (length % patternLen == 0)
+                    {
+                        string pattern = number.Substring(0, patternLen);
+                        int repeats = length / patternLen;
+                        string patternFull = String.Concat(Enumerable.Repeat(pattern, repeats));
+                        if (number == patternFull)
+                        {
+                            result += count;
+                            //Console.WriteLine("pattern " + pattern + " identified in " + patternFull);
+                            break;
+                        }
+                    }
+                }
+                count += 1;
+            }
+        }
+        Console.WriteLine("Answer for part 2: " + result);
+    }
+}
 
 var file = "day02_cs/input.txt";
+//var file = "day02_cs/test.txt";
 Part1(file);
+Part2(file);
