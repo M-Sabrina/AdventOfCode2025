424c3a86943b60dc27d1ed24d064ee2d14b5e06f
Sabrina Meindlhumer <58905866+M-Sabrina@users.noreply.github.com>
2025-12-06 18:29:39 +0100

    day 06 part 2 Python

--- day06_py/main.py
+++ day06_py/main.py
@@ -41,8 +41,55 @@ def part1(file: Path) -> None:
     print(f"Answer for part 1: {int(grand_total)}")
 
 
+def part2(file: Path) -> None:
+    input = []
+    with open(file) as f:
+        for line in f:
+            input.append(line.replace("\n", ""))
+    rows = len(input)
+    cols = len(input[0])
+    # print(input)
+
+    grand_total = 0
+
+    numbers = []
+    numbers_ind = 0
+    for col in range(cols):
+        symbol_col = input[rows - 1][col]
+        if not (symbol_col == " "):
+            symbol = symbol_col
+            # print(symbol)
+            numbers = []
+        val = ""
+        for row in range(rows - 1):
+            val += input[row][col]
+        if val.replace(" ", "") == "":  # empty column
+            numbers_ind = 0
+            if symbol == "+":
+                grand_total += sum(numbers)
+            else:
+                multiply_res = 1
+                for num in numbers:
+                    multiply_res *= num
+                grand_total += multiply_res
+            continue
+        numbers.append(int(val))
+        numbers_ind = numbers_ind + 1
+
+    # final one:
+    if symbol == "+":
+        grand_total += sum(numbers)
+    else:
+        multiply_res = 1
+        for num in numbers:
+            multiply_res *= num
+        grand_total += multiply_res
+
+    print(f"Answer for part 2: {int(grand_total)}")
+
+
 if __name__ == "__main__":
     file = Path("day06_py/input.txt")
     # file = Path("day06_py/test.txt")
     part1(file)
-    # part2(file)
+    part2(file)
