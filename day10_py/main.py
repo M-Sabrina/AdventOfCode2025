9af233a84f3e50982a904e53fb4efa9eebdc9f91
Sabrina Meindlhumer <58905866+M-Sabrina@users.noreply.github.com>
2025-12-13 23:04:58 +0100

    day 10 part 1 Python

--- day10_py/main.py
+++ day10_py/main.py
@@ -1,16 +1,46 @@
+from itertools import combinations_with_replacement
 from pathlib import Path
 
 
+def button_click(list1: list[int], list2: list[int]):
+    for ind in range(len(list1)):
+        list1[ind] = (list1[ind] + list2[ind]) % 2
+
+
 def part1(file: Path) -> None:
-    input = []
     # Each line contains a single indicator light diagram in [square brackets],
     # one or more button wiring schematics in (parentheses),
     # and joltage requirements in {curly braces}.
+    button_presses_total = 0
     with open(file) as f:
         for line in f:
-            input.append(line.strip())
+            line = line.strip()
+            indicator_diagram = line.split("]")[0][1:]
+            buttons = line.split("{")[0].split("]")[1].strip().split(" ")
+            indicators = [
+                0 if c == "." else 1 for c in indicator_diagram
+            ]  # ".##." -> 0110
+            masks = []
+            empty_mask = [0 for _ in indicators]
+            for ind, button in enumerate(buttons):
+                mask = empty_mask.copy()
+                nums = button[1:-1].split(",")
+                for num in nums:
+                    mask[int(num)] = 1
+                masks.append(mask)
 
-    print(f"Answer for part 1: {0}")
+            def solve():
+                for i in range(1, 10):
+                    for combination in combinations_with_replacement(masks, i):
+                        lights = empty_mask.copy()
+                        for mask in combination:
+                            button_click(lights, mask)
+                        if lights == indicators:
+                            return i
+
+            button_presses_total += solve()
+
+    print(f"Answer for part 1: {button_presses_total}")
 
 
 if __name__ == "__main__":
